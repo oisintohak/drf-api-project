09c76562c1d453a4b7e6d97e7875d03af5d2e528
oisintohak <oisintohak@gmail.com>
2023-02-24 11:05:47 +0000

    add profiles app and model

---
+++ profiles/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_qdjgyp'
+    )
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create a Profile when a new User is created
+    """
+    if created:
+        Profile.objects.create(user=instance)
