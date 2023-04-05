bc3cd8f15c93e431e675d42f432a4d9cb0333cfd
oisintohak <>
2023-04-05 15:58:16 +0000

    remove social auth urls

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from .views import FacebookLogin, GoogleLogin
 
 from dj_rest_auth import urls
 
@@ -11,9 +10,6 @@ urlpatterns = [
     path(
         'auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-    path('auth/facebook/', FacebookLogin.as_view(), name='fb_login'),
-    path('auth/google/', GoogleLogin.as_view(), name='google_login'),
-    path('accounts/', include('allauth.urls'), name='socialaccount_signup'),
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
     path('events/', include('events.urls')),
