63c3af721c5e1e130f8501579cf776d9a59bbe41
oisintohak <oisintohak@gmail.com>
2023-05-04 19:47:07 +0000

    create basic event and eventlist components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,9 @@
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
-import RegisterForm from "./pages/auth/Register";
+// import RegisterForm from "./pages/auth/Register";
 import LoginForm from "./pages/auth/Login";
+import Event from "./components/Event";
+import EventList from "./pages/events/EventList";
 
 function App() {
   return (
@@ -14,11 +16,12 @@ function App() {
           render={() => (
             <>
               <h1>home</h1>
+              <EventList />
             </>
           )}
         />
         <Route exact path="/login" render={() => <LoginForm />} />
-        <Route exact path="/register" render={() => <RegisterForm />} />
+        {/* <Route exact path="/register" render={() => <RegisterForm />} /> */}
       </Switch>
     </div>
   );
--- react-app/src/utils/utils.js
+++ react-app/src/utils/utils.js
@@ -1,7 +1,9 @@
 import jwtDecode from "jwt-decode";
 
 export const setTokenTimestamp = (data) => {
+  console.log(data)
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  console.log(refreshTokenTimestamp)
   localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
 };
 
