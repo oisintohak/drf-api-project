f8b2ef367fdc787f8012241116877b780bd43c6b
Oisin Tohak <oisintohak@gmail.com>
2023-08-19 16:15:03 +0100

    add wildcard url for react routing and updating static settings

--- main/settings.py
+++ main/settings.py
@@ -162,15 +162,15 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'staticfiles', 'build'),
-#     os.path.join(BASE_DIR, 'staticfiles', 'build', 'static'),
-# ]
+STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'staticfiles', 'build'),
+    os.path.join(BASE_DIR, 'staticfiles', 'build', 'static'),
+]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
 
 # Default primary key field type
--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
+from django.urls import re_path
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('auth/', include('dj_rest_auth.urls')),
     path(
         'auth/registration/', include('dj_rest_auth.registration.urls')
@@ -12,4 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
     path('events/', include('events.urls')),
+    # path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'.*', TemplateView.as_view(template_name='index.html'))  # only if the above routes don't trigger a match
 ]
