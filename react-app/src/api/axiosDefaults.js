5486dea10f4bcff71b93d0b2d4ff975564f1f208
Oisin Tohak <oisintohak@gmail.com>
2024-08-08 15:43:04 +0000

    restructure directories

--- react-app/src/api/axiosDefaults.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "api/";
-
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
-
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
--- vite/eventually/src/components/ProfileImage.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "react-bootstrap/Image";
-
-import React from "react";
-
-function ProfileImage({ src }) {
-  return <Image rounded src={src} width={40} height={40} />;
-}
-
-export default ProfileImage;
